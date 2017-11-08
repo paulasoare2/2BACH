2582e4274057236957e0f38e8929b376e5f322b1
paulasoare2 <33484925+paulasoare2@users.noreply.github.com>
2017-11-08 12:52:26 +0100

    Add files via upload

---
+++ GITHUB1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "LISTA DE PALABRAS"
+numero = int(input("Dígame cuántas palabras tiene la lista: "))
+
+if numero < 0:
+    print("¡Imposible!")
+else:
+    lista = []
+for i in range(numero):
+	print "Digame la palabra", str(i+1), ": "
+	palabra = raw_input()
+	lista += [palabra]
+print("La lista creada es:", lista)
